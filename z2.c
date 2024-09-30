c38981b454df5f36c6f8c46370aa28e3cd3c2f33
larke4 <73461164+larke4@users.noreply.github.com>
2024-09-30 21:50:56 +0700

    Update z2.c

--- z2.c
+++ z2.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 
-void main(){
-	int n = 2;
-	int flag = 0;
-	int a;
-	scanf("%d", &a);
-	while (a%n == 0){
-		if (n == a){
-			flag = 1;
-			break;
-		}
-	n++;
-	}
-	printf("%d \n", n);
+int main() {
+    int a;
+    scanf("%d", &a);
+    int n=2,
+    flag = 0;
+    while(a%n!=0) {
+        n++;
+        if (n==a) {flag = 1; break;};
+    }
+    printf("%d\n",n);
 }
+//ближайщее число делящиеся на 2 без остатка
